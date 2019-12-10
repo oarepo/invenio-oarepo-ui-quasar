87ca8022fba9221ddeb19a1ae94a058bb4b9e981
Miroslav Bauer <mirekys@gmail.com>
2019-12-10 17:18:01 +0100

    don't show collection when still loading data

--- src/components/OARepoQuasarCollection.vue
+++ src/components/OARepoQuasarCollection.vue
@@ -1,87 +1,87 @@
 <template>
 <oarepo-collection :collectionCode="collectionCode"
                    :locale="locale"
-                   ref="oarepoCollection">
-
+                   ref="oarepoCollection"
+>
     <template v-slot:default="{ collection, items, aggregations }">
-
-    <slot name="title" v-bind:collection="collection" v-bind:items="items" v-bind:aggregations="aggregations">
-        <h3 v-if="collection">{{ collection.title[locale] || collection.title.en }}</h3>
-    </slot>
-
-    <slot name="content-before" v-bind:collection="collection" v-bind:items="items">
-    </slot>
-
-    <div class="row horiz wrap items-stretch" v-if="items.length > 0">  <!-- use 'row' class to define a container / parent -->
-        <div class="col collection-content">
-
-            <slot name="list-before" v-bind:collection="collection">
+        <div v-if="!collectionLoading">
+            <slot name="title" v-bind:collection="collection" v-bind:items="items" v-bind:aggregations="aggregations">
+                <h3 v-if="collection">{{ collection.title[locale] || collection.title.en }}</h3>
             </slot>
 
-            <div v-if="omitList">
+            <slot name="content-before" v-bind:collection="collection" v-bind:items="items">
+            </slot>
 
-                <slot name="list-header" v-bind:collection="collection">
-                </slot>
+            <div class="row horiz wrap items-stretch" v-if="items.length > 0">  <!-- use 'row' class to define a container / parent -->
+                <div class="col collection-content">
 
-                <abstract-item v-for="item in items"
-                               :item="item"
-                               v-bind:key="`omit-${item.id}`"
-                               @selected="itemSelected(item)"
-                               :mapping="itemComponents"
-                               :omit-list="omitList"
-                >
-                </abstract-item>
+                    <slot name="list-before" v-bind:collection="collection">
+                    </slot>
+
+                    <div v-if="omitList">
+
+                        <slot name="list-header" v-bind:collection="collection">
+                        </slot>
+
+                        <abstract-item v-for="item in items"
+                                       :item="item"
+                                       v-bind:key="`omit-${item.id}`"
+                                       @selected="itemSelected(item)"
+                                       :mapping="itemComponents"
+                                       :omit-list="omitList"
+                        >
+                        </abstract-item>
+                    </div>
+                    <q-list v-else>
+
+                        <slot name="list-header" v-bind:collection="collection">
+                        </slot>
+
+                        <abstract-item v-for="item in items"
+                                       :item="item"
+                                       v-bind:key="item.id"
+                                       @selected="itemSelected(item)"
+                                       :mapping="itemComponents"
+                        >
+                        </abstract-item>
+                    </q-list>
+
+                    <q-pagination v-if="totalPages"
+                                  v-model="currentPage"
+                                  color="primary"
+                                  :max="totalPages"
+                                  :maxPages="9"
+                                  :boundaryNumbers="true"
+                                  :boundaryLinks="true"
+                                  :directionLinks="true"
+                                  class="pagination"
+                                  size="12px"
+                    ></q-pagination>
+
+                    <slot name="list-after" v-bind:collection="collection">
+                    </slot>
+
+                </div>  <!-- children will default to 'col'  -->
+                <div class="aggs collection-facets">
+                    <slot name="search-before" v-bind:collection="collection">
+                    </slot>
+                    <oarepo-quasar-search :input="searchString" class="q-mb-lg"></oarepo-quasar-search>
+                    <slot name="facets-before" v-bind:aggregations="aggregations" v-bind:collection="collection">
+                    </slot>
+                    <oarepo-quasar-facet-list :facets="aggregations">
+                        <template v-slot:facetCard="{ facetKey, values }">
+                           <slot name="facetCard" v-bind:facetKey="facetKey", v-bind:values="values"></slot>
+                        </template>
+                    </oarepo-quasar-facet-list>
+                    <slot name="facets-after" v-bind:collection="collection">
+                    </slot>
+                </div>
             </div>
-            <q-list v-else>
 
-                <slot name="list-header" v-bind:collection="collection">
-                </slot>
-
-                <abstract-item v-for="item in items"
-                               :item="item"
-                               v-bind:key="item.id"
-                               @selected="itemSelected(item)"
-                               :mapping="itemComponents"
-                >
-                </abstract-item>
-            </q-list>
-
-            <q-pagination v-if="totalPages"
-                          v-model="currentPage"
-                          color="primary"
-                          :max="totalPages"
-                          :maxPages="9"
-                          :boundaryNumbers="true"
-                          :boundaryLinks="true"
-                          :directionLinks="true"
-                          class="pagination"
-                          size="12px"
-            ></q-pagination>
-
-            <slot name="list-after" v-bind:collection="collection">
-            </slot>
-
-        </div>  <!-- children will default to 'col'  -->
-        <div class="aggs collection-facets">
-            <slot name="search-before" v-bind:collection="collection">
-            </slot>
-            <oarepo-quasar-search :input="searchString" class="q-mb-lg"></oarepo-quasar-search>
-            <slot name="facets-before" v-bind:aggregations="aggregations" v-bind:collection="collection">
-            </slot>
-            <oarepo-quasar-facet-list :facets="aggregations">
-                <template v-slot:facetCard="{ facetKey, values }">
-                   <slot name="facetCard" v-bind:facetKey="facetKey", v-bind:values="values"></slot>
-                </template>
-            </oarepo-quasar-facet-list>
-            <slot name="facets-after" v-bind:collection="collection">
+            <slot name="content-after" v-bind:collection="collection">
             </slot>
         </div>
-    </div>
-
-    <slot name="content-after" v-bind:collection="collection">
-    </slot>
     </template>
-
 </oarepo-collection>
 </template>
 
@@ -114,6 +114,10 @@ class OARepoCollection extends Vue {
     get searchString() {
         const q = new Query(this.$route.query);
         return q.get('q', true);
+    }
+
+    get collectionLoading() {
+        return this.oarepo$.collectionModule.state !== 2;
     }
 
     get totalPages() {
