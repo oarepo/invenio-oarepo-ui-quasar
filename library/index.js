042c7ada2ec98b2ba1fd13148aed2812d8c8683e
Mirek Simek <miroslav.simek@vscht.cz>
2019-12-30 10:34:21 +0100

    docker/k8s deployment

--- library/index.js
+++ library/index.js
@@ -1,8 +1,7 @@
 import OARepoCollectionList from './components/OARepoCollectionList.vue';
 
 export default {
-    install(Vue, options) {
-        Vue.component(OARepoCollectionList.name, OARepoCollectionList)
-        console.log(options)
+    install(Vue/*, options*/) {
+        Vue.component(OARepoCollectionList.name, OARepoCollectionList);
     }
 };
